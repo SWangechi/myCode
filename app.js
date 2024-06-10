4a762a944de1266520cb8bb384948525d1f92ae9
swangechi <sgituire@gmail.com>
2024-06-10 17:03:35 +0300

    2nd Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var msg = 'Hello world';
+console.log(msg)
+
+var a = 20
+var b = 30
+var c = a + b
+console.log(c)
+
+var msg1 = 'My name is Stella';
+var msg2 = 'I am running my first JavaScript file';
+console.log(msg1, msg2)
